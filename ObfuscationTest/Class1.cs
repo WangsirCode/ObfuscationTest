cff538a3609616fa4c8b0d5a5c2a33ec9047a841
bryant.wang <bryant.wang@dji.com>
2018-12-12 09:00:39 +0800

    init

---
+++ ObfuscationTest/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObfuscationTest
+{
+    public class ObfuscationTest
+    {
+        int number = 1;
+        string key = "public key"; 
+        private void encoding()
+        {
+            key = "encoding";
+        }
+
+        public void decoing()
+        {
+            key = "decoding";
+        }
+
+    }
+
+    class PrivateTest
+    {
+        public void testFunc()
+        {
+            string testString = "testString";
+        }
+    }
+}
